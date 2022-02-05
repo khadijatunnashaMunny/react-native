72fc3dfb21263157e0ae7aaf1c15d8035a9a440d
khadijatunnasha munny <khadijatunnashamunny@gmail.com>
2022-02-05 23:13:19 +0600

    categories done

--- Home.js
+++ Home.js
@@ -2,12 +2,14 @@ import { View,SafeAreaView, Text } from 'react-native';
 import React from 'react';
 import HeaderTab from './Components/HeaderTab';
 import SearchBar from './Components/SearchBar';
+import Categories from './Components/Categories';
 
 export default function Home() {
   return (
     <SafeAreaView>
       <HeaderTab/>
       <SearchBar/>
+      <Categories/>
     </SafeAreaView>
   );
 }
