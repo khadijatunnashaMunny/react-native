907e5f7eed5e4a13e42576224d3b587fdccfc248
khadijatunnasha munny <khadijatunnashamunny@gmail.com>
2022-02-05 21:28:06 +0600

    google search bar

--- Home.js
+++ Home.js
@@ -1,11 +1,13 @@
 import { View,SafeAreaView, Text } from 'react-native';
 import React from 'react';
 import HeaderTab from './Components/HeaderTab';
+import SearchBar from './Components/SearchBar';
 
 export default function Home() {
   return (
     <SafeAreaView>
       <HeaderTab/>
+      <SearchBar/>
     </SafeAreaView>
   );
 }
